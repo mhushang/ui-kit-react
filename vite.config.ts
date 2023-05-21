b195d4ca75826dd8c40ac181890241e45c0c2c75
mkhushang <mirzokhushang@ip-192-168-100-2.eu-central-1.compute.internal>
2023-05-21 17:15:44 +0500

    +

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"
 
 const fileName = {
   es: "index.mjs",
@@ -12,12 +13,22 @@ const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 module.exports = defineConfig({
   base: "./",
+  plugins: [react()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "UiKitReact",
       formats,
       fileName: (format) => fileName[format],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
